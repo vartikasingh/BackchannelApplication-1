c5c7f47b6f96042beef3cddc051f5a38923d279a
Vartika <vartikasingh07@gmail.com>
2011-10-18 18:58:51 -0400

    Updating integration test

--- test/integration/view_posts_test.rb
+++ test/integration/view_posts_test.rb
@@ -1,20 +1,23 @@
 require 'test_helper'
 
-class MyTest < Test::Unit::TestCase
+#class MyTest < Test::Unit::TestCase
+ class MyTest < ActionController::IntegrationTest
+
+  fixtures:all
 
   test "view posts from login page" do
   visit("/logins/new")
-  click_link("Login")
-  puts page.body
-
-  fill_in (:user, :with => "vsinghvar")
-  fill_in (:pass, :with => "vartikavar")
-  puts page.body
-  click_button("Login")
+  assert current_path == "/logins/new"
+  u=User.new("Unity_ID" => "vsingh3", "Name" =>"vartika", "Role" => "Student", "Password" => "vartika")
+  find_field("Username").set('vsingh3')
+  find_field("Password").set('vartika')
+  click_link_or_button("Login")
+  assert current_path == "/logins/new"
+  #assert current_path == "/posts/index"
   end
 
   test "go to new user page" do
-    visit("users_path")
+    visit("/logins/new")
     click_link("New user?")
     assert (current_path == "/users/new")
   end
